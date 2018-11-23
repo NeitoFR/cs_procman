7dff51784a3d84706905326f1243addf5f923997
Remousses <remy91490@hotmail.fr>
2018-11-23 12:21:22 +0100

    add amin right

--- cs_procman/Form1.Designer.cs
+++ cs_procman/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace cs_procman
-{
+{+
     partial class Form1
     {
         /// <summary>
@@ -84,6 +84,7 @@
             this.Controls.Add(this.name_dgv);
             this.Controls.Add(this.prop_dgv);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.prop_dgv)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.name_dgv)).EndInit();
